d867da4165702f7cb6fd443c58a570b2a8ee047a
Satanas-Gigas <120686392+Satanas-Gigas@users.noreply.github.com>
2023-06-30 15:51:15 +0500

    Update logger_1.py

--- logger_1.py
+++ logger_1.py
@@ -2,7 +2,6 @@
 import os
 from datetime import datetime
 
-
 def logger(old_function):
     def new_function(*args, **kwargs):
         result = old_function(*args, **kwargs)
@@ -32,14 +31,14 @@ def test_1():
     def div(a, b):
         return a / b
 
-    assert 'Hello World' == hello_world(), "Ôóíêöèÿ âîçâðàùàåò 'Hello World'"
+    assert 'Hello World' == hello_world(), "Функция возвращает 'Hello World'"
     result = summator(2, 2)
-    assert isinstance(result, int), 'Äîëæíî âåðíóòüñÿ öåëîå ÷èñëî'
+    assert isinstance(result, int), 'Должно вернуться целое число'
     assert result == 4, '2 + 2 = 4'
     result = div(6, 2)
     assert result == 3, '6 / 2 = 3'
 
-    assert os.path.exists(path), 'ôàéë main.log äîëæåí ñóùåñòâîâàòü'
+    assert os.path.exists(path), 'файл main.log должен существовать'
 
     summator(4.3, b=2.2)
     summator(a=0, b=0)
@@ -47,9 +46,9 @@ def test_1():
     with open(path) as log_file:
         log_file_content = log_file.read()
 
-    assert 'summator' in log_file_content, 'äîëæíî çàïèñàòüñÿ èìÿ ôóíêöèè'
+    assert 'summator' in log_file_content, 'должно записаться имя функции'
     for item in (4.3, 2.2, 6.5):
-        assert str(item) in log_file_content, f'{item} äîëæåí áûòü çàïèñàí â ôàéë'
+        assert str(item) in log_file_content, f'{item} должен быть записан в файл'
 
 
 if __name__ == '__main__':
